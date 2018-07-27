1789c25f90bd2e0291dc9a9aaafb09c39d626313
Roderik van der Veer <roderik@settlemint.com>
2018-07-27 14:26:58 +0200

    fix: build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
 if ([ $TRAVIS_BRANCH == "master" ]); then npm run semantic-release; fi
 yes Y | docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker build --build-arg NPM_TOKEN=${NPM_TOKEN} -t settlemint/bountyredeem-dapp:${TRAVIS_BRANCH} .
-docker push settlemint/bountyredeem-dapp
+docker build --build-arg NPM_TOKEN=${NPM_TOKEN} -t settlemint/lockedtokens-dapp:${TRAVIS_BRANCH} .
+docker push settlemint/lockedtokens-dapp
